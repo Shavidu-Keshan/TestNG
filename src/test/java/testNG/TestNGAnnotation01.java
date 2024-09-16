2d5775a6377e2448d6e857df769b93d02fc07bfc
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-16 17:13:17 +0530

    first commit

---
+++ src/test/java/testNG/TestNGAnnotation01.java
@@ -0,0 +1,25 @@
+package testNG;
+import org.testng.annotations.Test;
+public class TestNGAnnotation01 {
+    @Test(priority = 0)
+    public void testLogin(){
+        System.out.println("Login");
+    }
+    @Test(priority = 1)
+    public void testCreate(){
+        System.out.println("create");
+    }
+    @Test(priority = 2)
+    public void testEdit(){
+        System.out.println("Edit");
+    }
+    @Test(priority = 3)
+    public void testDelete(){
+        System.out.println("Delee");
+    }
+    @Test(priority = 4)
+    public void testLogout(){
+        System.out.println("LogOut");
+    }
+
+}
