4e616b32cb88f0a844cf9a466a27cca4123881a1
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-21 04:57:10 +0530

    complete grop test

--- src/test/java/testNG/GroupTest.java
+++ src/test/java/testNG/GroupTest.java
@@ -1,4 +1,28 @@
 package testNG;
 
+import org.testng.annotations.Test;
+
 public class GroupTest {
+    // S = smoke     // R= regresson
+
+    @Test(groups = {"S","R" })
+    public  void test01(){
+        System.out.println("smoke and regresion test case ");
+    }
+    @Test(groups = {"R" })
+    public  void test02(){
+        System.out.println("regresion test case ");
+    }
+    @Test
+    public  void test0(){
+        System.out.println("not smoke or regresion test case ");
+    }
+    @Test(groups = {"S"})
+    public  void test04(){
+        System.out.println("smoke  test case ");
+    }
+    @Test(groups = {"S","R" })
+    public  void test05(){
+        System.out.println("smoke and regresion test case ");
+    }
 }
