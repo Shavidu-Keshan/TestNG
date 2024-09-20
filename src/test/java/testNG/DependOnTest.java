202b3c5a381f75598f835a6a108e0560c2b4faf0
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-20 07:58:23 +0530

    create testngxml file

--- src/test/java/testNG/DependOnTest.java
+++ src/test/java/testNG/DependOnTest.java
@@ -1,4 +1,19 @@
 package testNG;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DependOnTest {
+    @Test()
+    public void olevel(){
+        System.out.println("ol");
+        Assert.fail();
+    }
+    @Test(dependsOnMethods = "olevel")
+    public void alevel(){
+        System.out.println("al");
+    }
+    @Test(dependsOnMethods = {"olevel","alevel"}, alwaysRun = true)
+    public void campus(){
+        System.out.println("campus");
+    }
 }
